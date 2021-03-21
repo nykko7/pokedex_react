3728ba3c118ab3bea806616ecb3b42e59343f02e
Nicolas P <n.perezbus@gmail.com>
2021-03-21 18:11:19 -0300

    feat: add pokeapi functions

---
+++ src/api/PokeAPI.js
@@ -0,0 +1,34 @@
+export const searchPokemon = async (pokemon) => {
+	try {
+		let url = `https://pokeapi.co/api/v2/pokemon/${pokemon}/`;
+		const response = await fetch(url);
+		const data = await response.json();
+		return data;
+	} catch (error) {}
+};
+
+export const getPokemons = async (limit = 25, offset = 0) => {
+	try {
+		let url = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`;
+		const response = await fetch(url);
+		const data = await response.json();
+		return data;
+	} catch (error) {}
+};
+
+export const getPokemonData = async (name) => {
+	const url = `https://pokeapi.co/api/v2/pokemon/${name}/`;
+	try {
+		let response = await fetch(url);
+		const data = await response.json();
+		return data;
+	} catch (err) {
+		try {
+			let response = await fetch(url.splice(url.length - 1));
+			const data = await response.json();
+			return data;
+		} catch (error) {
+			console.log(error);
+		}
+	}
+};
